f2560ef956f3f6af55d6f62c30bb49d68c7c7318
Noopur <noorweb3@outlook.com>
2024-11-15 22:38:46 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const bar = document.querySelector(".bar")
+    const closebar = document.querySelector(".closebar")
+    const navLinks = document.querySelector(".navlinks");
+
+    bar.addEventListener("click", () => {
+        navLinks.style.display = "flex"; 
+        bar.style.display = "none"; 
+        closebar.style.display = "block"; 
+    });
+
+    closebar.addEventListener("click", () => {
+        navLinks.style.display = "none"; 
+        bar.style.display = "block"; 
+        closebar.style.display = "none"; 
+    });
+})
+
+
+function toggleMenu() {
+    const bar = document.querySelector(".bar");
+    const closebar = document.querySelector(".closebar");
+    const navLinks = document.querySelector(".navlinks");
+
+
+    navLinks.classList.toggle('show');
+
+    if (navLinks.classList.contains('show')) {
+        bar.style.display = "none";
+        closebar.style.display = "block"; 
+    } else {
+        bar.style.display = "block";     
+        closebar.style.display = "none"; 
+    }
+}
+
